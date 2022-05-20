7b65c529362c5e88d8dec226a78d31eb421e5a4f
Danmazda <dan_mazda@hotmail.com>
2022-05-19 21:29:06 -0300

    delete and update forms

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,29 @@ const updateCounter = document.querySelector(".update");
 //Search
 const searchInput = document.querySelector("#search");
 
+//Login
+const loginBt = document.querySelector(".login");
+const loginMenu = document.querySelector(".loginMenu");
+const sendLoginCredential = document.querySelector(".sendLoginCredential");
+const email = document.querySelector("[type='email']");
+const password = document.querySelector("[type='password']");
+
+//Admin
+const adminSection = document.querySelector("#admin");
+const submitButton = document.querySelector("[type='submit']");
+loginBt.addEventListener("click", () => {
+  loginMenu.classList.toggle("hidden");
+});
+
+sendLoginCredential.addEventListener("click", async () => {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  console.log({ email: emailValue, password: passwordValue });
+  adminSection.classList.remove("hidden");
+  loginMenu.classList.toggle("hidden");
+});
 fetchPaletas();
+fetchPaletasAdmin();
 updateCart();
 
 cartLogo.addEventListener("click", () => {
@@ -40,14 +62,14 @@ async function fetchPaletas() {
   const allPaletas = await response.json();
   allPaletas.forEach((paleta) => {
     const htmlString = `<div class="container">
-    <img src="${paleta.foto}" alt="Paleta Sabor ${paleta.sabor}">
-    <h3>${paleta.sabor}</h3>
-    <p>${paleta.descricao}</p>
+    <img src="${paleta.image}" alt="Paleta Sabor ${paleta.flavor}">
+    <h3>${paleta.flavor}</h3>
+    <p>${paleta.description}</p>
     <div class="priceAndAdd">
-      <p class="price">R$${paleta.preco.toFixed(2)}</p>
-      <button onclick="PaletaIDCart(${
-        paleta.id
-      })"><i class="fa-solid fa-cart-plus"></i></button>
+      <p class="price">R$${paleta.price.toFixed(2)}</p>
+      <button onclick="PaletaIDCart('${
+        paleta._id
+      }')"><i class="fa-solid fa-cart-plus"></i></button>
     </div>
     </div>`;
     const div = document.createElement("div");
@@ -59,22 +81,27 @@ async function fetchPaletas() {
 async function PaletaIDCart(id, del = false) {
   let response;
   del
-    ? (response = await axios.post(`${baseUrl}user/deleteOne/${id}`))
-    : (response = await axios.post(`${baseUrl}user/add/${id}`));
-  const cart = response.data;
+    ? (response = await fetch(`${baseUrl}user/deleteOne/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: "souza@hotmail.com" }),
+      }))
+    : (response = await axios.post(`${baseUrl}user/add/${id}`, {
+        email: "souza@hotmail.com",
+      }));
   if (response.status === 200) {
-    let count = 0;
-    cart.forEach(p=>{
-      count += p.quantity;
-    });
-    updateCounter.innerText = `${count}`;
-    updateCounter.classList.remove("hidden");
-    setTimeout(() => {
-      updateCounter.classList.add("hidden");
-    }, 1000);
     updateCart();
   }
-  updateTotal(cart);
+}
+
+function showCounter(count) {
+  updateCounter.innerText = `${count}`;
+  updateCounter.classList.remove("hidden");
+  setTimeout(() => {
+    updateCounter.classList.add("hidden");
+  }, 1000);
 }
 
 function searchFor(query) {
@@ -88,56 +115,188 @@ function searchFor(query) {
 
 async function deleteAllCart(e, id) {
   e.parentElement.remove();
-  const response = axios.post(`${baseUrl}user/deleteAll/${id}`);
-  updateCart();
-}
-
-function updateTotal(userCart) {
-  let total = 0;
-  userCart.forEach((p) => {
-    total += p.preco * p.quantity;
+  const response = await fetch(`${baseUrl}user/deleteAll/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: "souza@hotmail.com" }),
   });
-  cartTotal.innerText = `Total: R$${total.toFixed(2)}`;
+  console.log(response);
+  updateCart();
 }
 
 async function fetchUserCart() {
   const response = await fetch(`${baseUrl}user/all`);
   const user = await response.json();
-  return user;
+  return user[1].list;
 }
 
 async function updateCart() {
   while (cartItems.lastChild) {
     cartItems.firstChild.remove();
   }
-  let userCart = await fetchUserCart();
-  if (userCart.length === 0 && !deleteOn) {
-    const deleteOn = document.createElement("p");
-    deleteOn.innerText = "No items in cart";
-    cartMenu.appendChild(deleteOn);
-  } else {
+
+  let user = await fetchUserCart();
+  if (user.length > 0) {
     deleteOn.remove();
   }
-  userCart.forEach((paleta) => {
-    const htmlString = `<div class="itemMenu">
-      <img src="${paleta.foto}" alt="Paleta Sabor ${paleta.sabor}">
-      <h3>${paleta.sabor}</h3>
-      <p class="price">R$${paleta.preco.toFixed(2)}</p>
-      <div class="counterHolder" key='${paleta.id}'>
-        <i class="fa-solid fa-plus" onclick="PaletaIDCart(${paleta.id})"></i>
-        <p class="quantity">${paleta.quantity}</p>
-        <i class="fa-solid fa-minus" onclick="PaletaIDCart(${
-          paleta.id
-        }, ${true})"></i>
+  const alreadyShownFlavors = [];
+  let total = 0;
+  user.forEach((paleta) => {
+    if (!alreadyShownFlavors.includes(paleta.flavor)) {
+      const quantity = user.filter((p) => p._id === paleta._id).length;
+      const htmlString = `<div class="itemMenu">
+      <img src="${paleta.image}" alt="Paleta Sabor ${paleta.flavor}">
+      <h3>${paleta.flavor}</h3>
+      <p class="price">R$${paleta.price.toFixed(2)}</p>
+      <div class="counterHolder" key='${paleta._id}'>
+        <i class="fa-solid fa-plus" onclick="PaletaIDCart('${paleta._id}')"></i>
+        <p class="quantity">${quantity}</p>
+        <i class="fa-solid fa-minus" onclick="PaletaIDCart('${
+          paleta._id
+        }', ${true})"></i>
       </div>
-      <button onclick="deleteAllCart(this, ${
-        paleta.id
-      })"><i class="fa-solid fa-trash-can"></i></button>
+      <button onclick="deleteAllCart(this, '${
+        paleta._id
+      }')"><i class="fa-solid fa-trash-can"></i></button>
     </div>`;
+      const div = document.createElement("div");
+      div.innerHTML = htmlString;
+      cartItems.insertAdjacentElement("afterbegin", div);
+      cartItems.classList.remove("hidden");
+      alreadyShownFlavors.push(paleta.flavor);
+      total += paleta.price * quantity;
+    }
+  });
+  showCounter(user.length);
+  cartTotal.innerText = `Total: R$${total.toFixed(2)}`;
+}
+
+//admin
+async function fetchPaletasAdmin() {
+  while (document.querySelector(".container")) {
+    document.querySelector(".container").remove();
+  }
+  const response = await fetch(`${baseUrl}paletas/all`);
+  const allPaletas = await response.json();
+  allPaletas.forEach((paleta) => {
+    const htmlString = `<div class="container">
+    <img src="${paleta.image}" alt="Paleta Sabor ${paleta.flavor}">
+    <h3>${paleta.flavor}</h3>
+    <p>${paleta.description}</p>
+    <p class="price">R$${paleta.price.toFixed(2)}</p>
+    <p>ID: ${paleta._id}</p>
+    <div class="paletaControl">
+      <button onclick="updatePaleta(${paleta})">Update</button>
+      <button onclick="deletePaleta('${paleta._id}')">Delete</button>
+    </div>
+    </div>
+    `;
+    const paletaUpdateForm = `<form action="/" method="post" class="hidden">
+    <h3>Update:</h3>
+    <fieldset>
+      <label for="flavor"> Flavor</label>
+      <input type="text" name="flavor" required value="${paleta.flavor}"/>
+    </fieldset>
+    <fieldset>
+      <label for="image"> Image Url</label>
+      <input type="text" name="image" required value="${paleta.image}"/>
+    </fieldset>
+    <fieldset>
+      <label for="description"> Description</label>
+      <input type="text" name="description" required  value="${paleta.description}"/>
+    </fieldset>
+    <fieldset>
+      <label for="price"> Price</label>
+      <input type="number" name="price" min="1" max="100" step="0.1" required value="${Number(paleta.price)}"/>
+    </fieldset>
+    <button type="submit">Send</button>
+  </form>`;
+    const divForm = document.createElement("div");
+    divForm.innerHTML = paletaUpdateForm;
     const div = document.createElement("div");
     div.innerHTML = htmlString;
-    cartItems.insertAdjacentElement("afterbegin", div);
-    cartItems.classList.remove("hidden");
+    div.appendChild(divForm);
+    adminSection.insertAdjacentElement("beforeend", div);
   });
-  updateTotal(userCart);
+}
+async function logOut() {
+  adminSection.classList.add("hidden");
+}
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const reqBody = {
+    price: formData.get("price"),
+    image: formData.get("image"),
+    description: formData.get("description"),
+    flavor: formData.get("flavor"),
+  };
+  const response = await fetch(`${baseUrl}paletas/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqBody),
+  });
+  if (response.status === 200) {
+    Swal.fire({
+      title: "Paleta Created!",
+      icon: "success",
+      confirmButtonText: "Cool",
+    });
+  } else {
+    Swal.fire({
+      title: "Error!",
+      text: "Failed to create paleta",
+      icon: "error",
+      confirmButtonText: "Cool",
+    });
+  }
+  fetchPaletas();
+  fetchPaletasAdmin();
+});
+
+async function deletePaleta(id) {
+  const modal = await Swal.fire({
+    title: "Delete cannot be reversed",
+    text: "are you sure you want to delete?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  });
+  if (modal.isConfirmed) {
+    const response = await fetch(`${baseUrl}paletas/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 200) {
+      Swal.fire({
+        title: "Paleta Deleted!",
+        icon: "success",
+        confirmButtonText: "Cool",
+      });
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Failed to delete paleta",
+        icon: "error",
+        confirmButtonText: "Cool",
+      });
+    }
+    fetchPaletas();
+    fetchPaletasAdmin();
+  }
+}
+async function updatePaleta(paleta) {
+  const htmlString = ``;
+  const div = document.createElement("div");
+  div.innerHTML = htmlString;
+  document.body.appendChild(div);
 }
